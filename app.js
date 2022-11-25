ae8396607cce8723d3cb400f6b56b5ac8cc7a1ce
realsarius <tehadro@protonmail.com>
2022-11-25 19:55:01 +0300

    added UI for our game

--- app.js
+++ app.js
@@ -3,29 +3,65 @@ getComputerChoice = () => {
     return rps[Math.floor(Math.random() * rps.length)];
 }
 
+let playerScore = 0;
+let computerScore = 0;
+
 playRound = (playerSelection, computerSelection = getComputerChoice()) => {
     if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "PAPER") {
-        console.log("You Lose! Paper beats Rock");
+        resultsDiv.textContent = "You Lose! Paper beats Rock";
+        computerScore++;
     } else if (playerSelection.toUpperCase() === "ROCK" && computerSelection.toUpperCase() === "SCISSORS") {
-        console.log("You Win! Rock beats Scissors");
+        resultsDiv.textContent = "You Win! Rock beats Scissors";
+        playerScore++;
     } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "ROCK") {
-        console.log("You Win! Paper beats Rock");
+        resultsDiv.textContent = "You Win! Paper beats Rock";
+        playerScore++;
     } else if (playerSelection.toUpperCase() === "PAPER" && computerSelection.toUpperCase() === "SCISSORS") {
-        console.log("You Lose! Scissors beats Paper");
+        resultsDiv.textContent = "You Lose! Scissors beats Paper";
+        computerScore++;
     } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "ROCK") {
-        console.log("You Lose! Rock beats Scissors");
+        resultsDiv.textContent = "You Lose! Rock beats Scissors";
+        computerScore++;
     } else if (playerSelection.toUpperCase() === "SCISSORS" && computerSelection.toUpperCase() === "PAPER") {
-        console.log("You Win! Scissors beats Paper");
+        resultsDiv.textContent = "You Win! Scissors beats Paper";
+        playerScore++;
     } else {
-        console.log(`Draw ${playerSelection} versus ${computerSelection}`);
+        resultsDiv.textContent = `Draw! ${playerSelection} versus ${computerSelection}`;
+    }
+    score.textContent = `Player ${playerScore} | ${computerScore} Computer`
+    if (playerScore === 5) {
+        resultsDiv.style.textAlign = "center";
+        resultsDiv.textContent = "PLAYER WINS!"
+        btnRock.disabled = true;
+        btnPaper.disabled = true;
+        btnScissors.disabled = true;
+    }
+    if (computerScore === 5) {
+        resultsDiv.style.textAlign = "center";
+        resultsDiv.textContent = "COMPUTER WINS!"
+        btnRock.disabled = true;
+        btnPaper.disabled = true;
+        btnScissors.disabled = true;
     }
 }
 
-game = () => {
-    for (let i = 0; i < 5; i++) {
-        playRound(prompt("Enter input"));
-    }
+const btnRock = document.querySelector(".rock");
+const btnPaper = document.querySelector(".paper");
+const btnScissors = document.querySelector(".scissors");
+const body = document.querySelector("body");
 
-}
+btnRock.addEventListener('click', () => {
+    playRound("Rock", getComputerChoice());
+});
+btnPaper.addEventListener('click', () => {
+    playRound("Paper", getComputerChoice());
+});
+btnScissors.addEventListener('click', () => {
+    playRound("Scissors", getComputerChoice());
+});
 
-game();
+const resultsDiv = document.createElement("div");
+const score = document.createElement("h2");
+score.style.fontWeight = "bold";
+body.appendChild(resultsDiv);
+body.prepend(score);
